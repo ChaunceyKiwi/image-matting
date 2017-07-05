79aeea579fed831449290ae78b94473da8bb4c65
ChaunceyKiwi <chaunceykiwi@gmail.com>
2017-07-04 23:02:44 -0700

    cmake added to the project

---
+++ code/make/CMakeFiles/imageMatting.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Chauncey/Desktop/imageMatting/src/AlphaCalculator.cpp" "/Users/Chauncey/Desktop/imageMatting/make/CMakeFiles/imageMatting.dir/Users/Chauncey/Desktop/imageMatting/src/AlphaCalculator.cpp.o"
+  "/Users/Chauncey/Desktop/imageMatting/src/Image.cpp" "/Users/Chauncey/Desktop/imageMatting/make/CMakeFiles/imageMatting.dir/Users/Chauncey/Desktop/imageMatting/src/Image.cpp.o"
+  "/Users/Chauncey/Desktop/imageMatting/src/ImagePrinter.cpp" "/Users/Chauncey/Desktop/imageMatting/make/CMakeFiles/imageMatting.dir/Users/Chauncey/Desktop/imageMatting/src/ImagePrinter.cpp.o"
+  "/Users/Chauncey/Desktop/imageMatting/src/ImageReader.cpp" "/Users/Chauncey/Desktop/imageMatting/make/CMakeFiles/imageMatting.dir/Users/Chauncey/Desktop/imageMatting/src/ImageReader.cpp.o"
+  "/Users/Chauncey/Desktop/imageMatting/src/LaplacianCalculator.cpp" "/Users/Chauncey/Desktop/imageMatting/make/CMakeFiles/imageMatting.dir/Users/Chauncey/Desktop/imageMatting/src/LaplacianCalculator.cpp.o"
+  "/Users/Chauncey/Desktop/imageMatting/src/MattingPerformer.cpp" "/Users/Chauncey/Desktop/imageMatting/make/CMakeFiles/imageMatting.dir/Users/Chauncey/Desktop/imageMatting/src/MattingPerformer.cpp.o"
+  "/Users/Chauncey/Desktop/imageMatting/src/SparseMatrixEquationSolver.cpp" "/Users/Chauncey/Desktop/imageMatting/make/CMakeFiles/imageMatting.dir/Users/Chauncey/Desktop/imageMatting/src/SparseMatrixEquationSolver.cpp.o"
+  "/Users/Chauncey/Desktop/imageMatting/src/main.cpp" "/Users/Chauncey/Desktop/imageMatting/make/CMakeFiles/imageMatting.dir/Users/Chauncey/Desktop/imageMatting/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
